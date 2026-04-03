4ba1e20dd09dab806c30fd279e5a1ebb346303ce
bible <biblely0217@gmail.com>
2026-04-03 09:42:24 +0900

    3-1

--- code/3-1.c
+++ code/3-1.c
@@ -2,9 +2,8 @@
 
 void read_only(const int *arr, int size) {
     for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+        printf("%d\n", *(arr + i));
     }
-    printf("\n");
 }
 
 int main() {
