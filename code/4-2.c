24202982195192956efe9c54fb373c81ee9183a9
bible <biblely0217@gmail.com>
2026-04-03 09:46:29 +0900

    4-2

---
+++ code/4-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void echo() {
+    char buf[5];
+    fgets(buf, sizeof(buf), stdin);
+    printf("%s", buf);
+}
+
+int main() {
+    int x = 42;
+    echo();
+    return 0;
+}
